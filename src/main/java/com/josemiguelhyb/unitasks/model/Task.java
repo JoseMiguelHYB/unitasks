42d30db1d6f97564cfce5e01b9c6cfda4a97ae7b
José Miguel Castro Huayllas <JoseMiguelHYB@gmail.com>
2025-09-02 13:11:18 +0200

    actualizado
    sadasd

--- src/main/java/com/josemiguelhyb/unitasks/model/Task.java
+++ src/main/java/com/josemiguelhyb/unitasks/model/Task.java
@@ -1,25 +1,67 @@
 package com.josemiguelhyb.unitasks.model;
 
-import java.time.LocalDate;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="tasks")
-@Data       // Ya genera los getters, setters, toString(), equals, hashCode
-@NoArgsConstructor // Crea un constructor vacío (lo necesita JPA)
-@AllArgsConstructor // Crea un constructor con todos los campos
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     private String title;
 
     private boolean completed = false;
 
     private LocalDate dueDate; // Fecha de vencimiento
 
+    public Task() { }
+
+    public Task(String title, boolean completed, LocalDate dueDate) {
+        this.title = title;
+        this.completed = completed;
+        this.dueDate = dueDate;
+    }   
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	@Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", completed=" + completed +
+                ", dueDate=" + dueDate +
+                '}';
+    }
 }
